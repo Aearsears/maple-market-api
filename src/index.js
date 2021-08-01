8ef14b0c6f7df6b66e12c48a78713ad97b4a3643
Aearsears <53102185+Aearsears@users.noreply.github.com>
2021-08-01 01:14:01 -0400

    started api for images and routes for images

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('../db/index')
 const cors = require('cors')
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 app.use(cors());
@@ -13,6 +14,37 @@ app.get('/api/test',(req,resp)=>{
             return next(err);
         }
         resp.send(res.rows);
+    })
+});
+
+app.get('/api/img/:price',(req,resp)=>{
+    db.query("SELECT * FROM items WHERE price= "+ req.params.price,(err,res)=>{
+        if(err){
+            resp.status(404);
+            resp.send("Error!");
+        }
+        if(res.rows.length==0){
+            resp.status(200);
+            resp.send("nothing!");
+            
+        }
+        // console.log(res.rows['img_path']);
+        // resp.send(res.rows[0]['img_path']);
+        var options = {
+            root: path.join(__dirname+'/../'),
+            dotfiles: 'deny',
+            headers: {
+              'x-timestamp': Date.now(),
+              'x-sent': true
+            }
+          }
+        resp.sendFile(res.rows[0]['img_path'],options,(err)=>{
+            if (err) {
+                console.log(err);
+              } else {
+                console.log('Sent:', res.rows[0]['name'])
+              }
+        });
     })
 });
 
