656af5d76963e4ca5b8d9106cc17375c1dbc82d4
Tang <charlie.tang@accenture.com>
2021-08-24 18:35:06 -0400

    added mockdata for the price changes and added statements for database or mockdata

--- src/index.js
+++ src/index.js
@@ -9,68 +9,85 @@ const app = express();
 app.use(cors());
 app.use(morgan("combined"));
 
-app.get("/api/test", (req, resp) => {
+if (process.env.DATABASE === "false") {
+  app.get("/api/test", (req, resp) => {
     // console.log();
     resp.sendFile(path.join(__dirname, "/../db/mockdata.json"));
-});
+  });
 
-app.get("/api/mesomarket", (req, resp) => {
+  app.get("/api/mesomarket", (req, resp) => {
     // console.log();
     resp.sendFile(path.join(__dirname, "/../db/mesomarket.json"));
-});
+  });
+  
+  app.get("/api/img/:path", (req, resp) => {
+    resp.sendFile(path.join(__dirname, "/../public/img/",req.params.path));
 
-app.get("/api/img/:price", (req, resp) => {
-  db.query(
-    "SELECT * FROM items WHERE price= " + req.params.price,
-    (err, res) => {
+  });
+} 
+else {
+  app.get("/api/test", (req, resp) => {
+    db.query("SELECT * FROM items", (err, res) => {
       if (err) {
-        resp.status(404);
-        resp.send("Error!");
+        return next(err);
       }
-      //if there is no item in database
-      else if (res.rows.length == 0) {
-        resp.status(200);
-        resp.send("nothing!");
-      }
-      //check if no entry for img path in the database
-      else if (res.rows[0]["img_path"] == null) {
-        resp.status(404);
-        resp.send("Internal Database Error!");
-      } else {
-        console.log(res.rows);
-        console.log(path.join(__dirname, "/../", res.rows[0]["img_path"]));
-        //check if img file exists on filesystem
-        fs.stat(
-          path.join(__dirname, "/../", res.rows[0]["img_path"]),
-          function (err, stat) {
-            if (err == null) {
-              // file does exist
-              var options = {
-                root: path.join(__dirname + "/../"),
-                dotfiles: "deny",
-                headers: {
-                  "x-timestamp": Date.now(),
-                  "x-sent": true,
-                },
-              };
-              resp.sendFile(res.rows[0]["img_path"], options, (err) => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log("Sent:", res.rows[0]["name"]);
-                }
-              });
-            } else if (err.code === "ENOENT") {
-              // file does not exist
-              resp.status(200);
-              resp.send("no img file!");
+      resp.send(res.rows);
+    });
+  });
+
+  app.get("/api/img/:price", (req, resp) => {
+    db.query(
+      "SELECT * FROM items WHERE price= " + req.params.price,
+      (err, res) => {
+        if (err) {
+          resp.status(404);
+          resp.send("Error!");
+        }
+        //if there is no item in database
+        else if (res.rows.length == 0) {
+          resp.status(200);
+          resp.send("nothing!");
+        }
+        //check if no entry for img path in the database
+        else if (res.rows[0]["img_path"] == null) {
+          resp.status(404);
+          resp.send("Internal Database Error!");
+        } else {
+          console.log(res.rows);
+          console.log(path.join(__dirname, "/../", res.rows[0]["img_path"]));
+          //check if img file exists on filesystem
+          fs.stat(
+            path.join(__dirname, "/../", res.rows[0]["img_path"]),
+            function (err, stat) {
+              if (err == null) {
+                // file does exist
+                var options = {
+                  root: path.join(__dirname + "/../"),
+                  dotfiles: "deny",
+                  headers: {
+                    "x-timestamp": Date.now(),
+                    "x-sent": true,
+                  },
+                };
+                resp.sendFile(res.rows[0]["img_path"], options, (err) => {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    console.log("Sent:", res.rows[0]["name"]);
+                  }
+                });
+              } else if (err.code === "ENOENT") {
+                // file does not exist
+                resp.status(200);
+                resp.send("no img file!");
+              }
             }
-          }
-        );
+          );
+        }
       }
-    }
-  );
-});
+    );
+  });
+}
 
 app.listen(4000, (err) => {
   console.log(err);
