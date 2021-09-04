47c1c3fe8e3aaf2cf82b2e63798d37ff8686f29a
Aearsears <53102185+Aearsears@users.noreply.github.com>
2021-09-04 16:26:35 -0400

    added cors policy for front end url

--- src/server.js
+++ src/server.js
@@ -35,6 +35,14 @@ if (environment.trim() == "development") {
         resp.sendFile(path.join(__dirname, "/../db/mesomarket.json"));
     });
 } else {
+    const cors = require("cors");
+    var corsOptions={
+        origin:'https://maple-market-db.herokuapp.com',
+        optionsSuccessStatus:200
+    };
+    app.use(cors(corsOptions));
+
+
     require('./auth')();
     
     app.use(
